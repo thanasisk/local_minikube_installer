36cb2eaabdfaca54a810f2d559c15c393a5040ed
Athanasios Kostopoulos <athanasios@akostopoulos.com>
2020-10-17 20:28:40 +0200

    still WiP but a lot of pieces in order

--- installer.py
+++ installer.py
@@ -2,20 +2,34 @@
 
 import os
 import sys
+import shutil
 import requests
 
 def safe_write(filename: str, content: bytes):
-    pass
+    if os.path.isfile(filename) or os.path.isdir(filename) or os.path.islink(filename):
+        print("file exists or is dir or is link - aborting!")
+        sys.exit(2)
+    with open(filename, "wb") as ifile:
+        ifile.write(content)
+
+
+def safe_move(src: str, dst: str):
+    if os.path.isdir(src) or os.path.islink(src):
+        print("source file is not a file")
+        sys.exit(2)
+    shutil.copy(src,dst)
 
 def get_minikube():
     # latest and greatest!
     latest_URL = "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
     try:
         r = requests.get(latest_URL)
-        safe_write("kubectl", r.content)
-    except: # TODO: specific error
-    #sudo install minikube-linux-amd64 /usr/local/bin/minikube
-    pass
+        safe_write("minikube", r.content)
+    except e: # TODO: specific error
+        print(e)
+    safe_move("kubectl", "/usr/local/bin/minikube")
+    os.chown("/usr/local/bin/minikube",0,0)
+    os.chmod("/usr/local/bin/minikube",0755)
 
 def get_kubectl():
     #curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -29,4 +43,6 @@ def are_we_root():
        sys.exit(1)
 
 def are_we_on_linux():
-    pass
+    if not sys.platform.startswith("linux"):
+        print("Only supported on Linux")
+        sys.exit(1)
